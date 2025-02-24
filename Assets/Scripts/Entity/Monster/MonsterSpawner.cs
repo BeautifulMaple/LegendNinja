3a847b41a94e9f2eabcd36ea7a9c045e2ef45bfc
yndoo <isabelle2k@naver.com>
2025-02-24 17:35:30 +0900

    [ Chore ] MonsterSpawner에서 fileName 넣는 코드 수정

--- Assets/Scripts/Entity/Monster/MonsterSpawner.cs
+++ Assets/Scripts/Entity/Monster/MonsterSpawner.cs
@@ -8,10 +8,17 @@ public class MonsterSpawner : MonoBehaviour
     private int stage = 1;
     private int curWave = 1;
 
+    private string fileName = "";
     private MonsterDatabase monsterDB;
 
     void Start()
     {
-        monsterDB = DataTableLoader.LoadMonsterData("Stage1_MonsterTable");
+        fileName = $"Stage{stage}_MonsterTable";
+        monsterDB = DataTableLoader.LoadMonsterData(fileName);
     }   
+
+    //void RandomSpawn(int sNum, int mNum)
+    //{
+ 
+    //}
 }
